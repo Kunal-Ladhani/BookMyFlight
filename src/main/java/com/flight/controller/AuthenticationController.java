be8d936771408cb42ba1e49c7b2435f75f4f5307
Kunal Ladhani <kunal.ladhani@goniyo.com>
2024-11-15 18:02:04 +0530

    test  commit

--- src/main/java/com/flight/controller/AuthenticationController.java
+++ src/main/java/com/flight/controller/AuthenticationController.java
@@ -12,82 +12,73 @@ import com.flight.model.User;
 import com.flight.service.AuthenticationService;
 import com.flight.utils.HttpUtils;
 import jakarta.validation.Valid;
-import java.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/v1/auth")
 @CrossOrigin(origins = "*")
 public class AuthenticationController {
 
-  @Autowired
-  private AuthenticationService authenticationService;
+    @Autowired
+    private AuthenticationService authenticationService;
 
-  @Autowired
-  private HttpUtils httpUtils;
+    @Autowired
+    private HttpUtils httpUtils;
 
-  @PostMapping(value = "/sign-up", produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<SignUpResponseDto> userSignUp(
-      @Valid @RequestBody SignUpRequestDto signUpRequestDto) throws Exception {
-    return new ResponseEntity<>(authenticationService.userSignUp(signUpRequestDto),
-        HttpStatus.CREATED);
-  }
+    @PostMapping(value = "/sign-up", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<SignUpResponseDto> userSignUp(
+            @Valid @RequestBody SignUpRequestDto signUpRequestDto) throws Exception {
+        return new ResponseEntity<>(authenticationService.userSignUp(signUpRequestDto), HttpStatus.CREATED);
+    }
 
-  @PostMapping("/login")
-  public ResponseEntity<SessionDTO> userSignIn(@Valid @RequestBody UserDTO user)
-      throws UserAlreadyExistsException, InvalidCredentialException {
-    SessionDTO sdt = authenticationService.userLogin(user);
-    return new ResponseEntity<>(sdt, HttpStatus.OK);
-  }
+    @PostMapping("/log-in")
+    public ResponseEntity<SessionDTO> userSignIn(@Valid @RequestBody UserDTO user)
+            throws UserAlreadyExistsException, InvalidCredentialException {
+        SessionDTO sdt = authenticationService.userLogin(user);
+        return new ResponseEntity<>(sdt, HttpStatus.OK);
+    }
 
-  @PostMapping("/logout")
-  public ResponseEntity<MessageDTO> userLogout(@RequestParam(value = "key") String authKey)
-      throws InvalidCredentialException {
-    MessageDTO message = new MessageDTO();
-    String msg = authenticationService.userLogout(authKey);
-    message.setMessage(msg);
-    message.setTimestamp(LocalDateTime.now());
-    return new ResponseEntity<>(message, HttpStatus.OK);
-  }
+    @PostMapping("/logout")
+    public ResponseEntity<MessageDTO> userLogout(@RequestParam(value = "key") String authKey)
+            throws InvalidCredentialException {
+        MessageDTO message = new MessageDTO();
+        String msg = authenticationService.userLogout(authKey);
+        message.setMessage(msg);
+        message.setTimestamp(LocalDateTime.now());
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
 
-  @PutMapping("/profile")
-  public ResponseEntity<String> updateUser(@Valid @RequestBody User user)
-      throws InvalidCredentialException {
-    authenticationService.updateUser(user);
-    return new ResponseEntity<>("User updated successfully...", HttpStatus.ACCEPTED);
-  }
+    @PutMapping("/profile")
+    public ResponseEntity<String> updateUser(@Valid @RequestBody User user)
+            throws InvalidCredentialException {
+        authenticationService.updateUser(user);
+        return new ResponseEntity<>("User updated successfully...", HttpStatus.ACCEPTED);
+    }
 
-  @DeleteMapping("/delete")
-  public ResponseEntity<User> deleteUser(@RequestParam Integer userid, @RequestParam String authKey)
-      throws InvalidCredentialException {
-    User u = authenticationService.deleteUser(userid, authKey);
-    return new ResponseEntity<>(u, HttpStatus.OK);
-  }
+    @DeleteMapping("/delete")
+    public ResponseEntity<User> deleteUser(@RequestParam Integer userid, @RequestParam String authKey)
+            throws InvalidCredentialException {
+        User u = authenticationService.deleteUser(userid, authKey);
+        return new ResponseEntity<>(u, HttpStatus.OK);
+    }
 
-  @PostMapping("/appoint")
-  public ResponseEntity<User> appointNewAdmin(@RequestParam("email") String email,
-      @RequestParam("code") String passcode) throws InvalidCredentialException {
-    User user = authenticationService.makeUserAdmin(email, passcode);
-    return new ResponseEntity<>(user, HttpStatus.OK);
-  }
+    @PostMapping("/appoint")
+    public ResponseEntity<User> appointNewAdmin(@RequestParam("email") String email,
+                                                @RequestParam("code") String passcode) throws InvalidCredentialException {
+        User user = authenticationService.makeUserAdmin(email, passcode);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
 
-  @GetMapping("/test/{id}")
-  public ResponseEntity<ProductDto> test(@PathVariable("id") String productId) {
-    ResponseEntity<ProductDto> response = httpUtils.get(
-        "https://fakestoreapi.com/products/" + productId, null, null, ProductDto.class);
-    return ResponseEntity.ok(response.getBody());
-  }
+    @GetMapping("/test/{id}")
+    public ResponseEntity<ProductDto> test(@PathVariable("id") String productId) {
+        ResponseEntity<ProductDto> response = httpUtils.get(
+                "https://fakestoreapi.com/products/" + productId, null, null, ProductDto.class);
+        return ResponseEntity.ok(response.getBody());
+    }
 }
